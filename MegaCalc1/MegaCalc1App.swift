7ebbaebcfac342372e13e7736a0a26597c05e0ba
Francois Robert <frobert@pixtolab.com>
2021-12-27 20:26:16 -0500

    Initial Commit

---
+++ MegaCalc1/MegaCalc1App.swift
@@ -0,0 +1,17 @@
+//
+//  MegaCalc1App.swift
+//  MegaCalc1
+//
+//  Created by Francois Robert on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct MegaCalc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
